dae7826d49158f3c63d4e4189ce3a40caa649bc5
VICTOR GABRIEL RODRIGUES LINS <gabrielvghs666@gmail.com>
2023-07-10 17:57:07 -0300

    Add files via upload

---
+++ CardTask.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="boxCard">
+    <div class="card">
+      <p class="style-for-card"></p>
+      <p>
+        {{ $props.title }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CardTask',
+  props: {
+    title: String
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .boxCard {
+    padding-top: 1rem;
+    width: 100%;
+  }
+  .card {
+    background-color: cyan;
+    padding: 1rem;
+    width: 100%;
+  }
+</style>
---
+++ Form.vue
@@ -0,0 +1,62 @@
+<template>
+  <form class="section-group">
+    <input 
+      type="text"
+      class="input-text"
+      id="title" 
+      placeholder="EX: fazer relatório"
+      v-model="title">
+    <button 
+      type="button"
+      @click="createTask"
+      class="button-create"
+    >
+      Criar tarefa
+    </button>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: "FormTask",
+    emits: ["saveTask"],
+    data() {
+      return {
+        title: ''
+      }
+    },
+    methods: {
+      createTask() {
+        this.$emit("saveTask", this.title)
+        this.title = ""
+      }
+    }
+  })
+</script>
+
+<style scoped>
+  .input-text {
+    padding: .5rem;
+    border-radius: .3rem;
+    font-size: .9rem;
+    width: 20rem;
+  }
+  .button-create {
+    padding: .7rem;
+    border-radius: .3rem;
+    font-size: .9rem;
+    border: none;
+    background-color: cyan;
+    cursor: pointer;
+  }
+  .button-create:hover {
+    opacity: 0.86;
+  }
+  .section-group {
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+  }
+</style>
